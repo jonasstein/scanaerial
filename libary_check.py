7661aa8eab7d8ca3cfe6fe7d56f88ccb72cea955
Bürste <cyrond@gmail.com>
2011-02-13 16:08:06 +0100

    creating libary_check.py
    Signed-off-by: Bürste <cyrond@gmail.com>

---
+++ libary_check.py
@@ -0,0 +1,45 @@
+﻿#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+"""
+Use this to detect missing libaries which are needed by fuzzer.
+"""
+
+import sys
+
+def debug(st):
+    sys.stderr.write(str(st)+"\n")
+    
+check = True
+
+debug("Checking Python Image Libary")
+try:
+    import Image, ImageFilter
+    debug("Found.")
+except ImportError:
+    check = False
+    debug("PIL missing, download at http://www.pythonware.com/products/pil/")
+
+debug("Checking Python Projection Libary")
+try:
+    import pyproj
+    debug("Found.")
+except ImportError:
+    check = False
+    debug("Python Projection Libary missing, download at http://code.google.com/p/pyproj/")
+    
+if check:
+    print("Everthing works fine, you may run fuzzer now.")
