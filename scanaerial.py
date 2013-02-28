cf36b73144e56c86d82efd03a2a7e7309a14161b
tilmanb <tilman.brock@googlemail.com>
2013-02-28 15:40:58 +0100

    Update scanaerial.py
    Spelling fix ("a area" -> "an area")

--- scanaerial.py
+++ scanaerial.py
@@ -15,7 +15,7 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 """
-This script will search a area with similar color and send
+This script will search an area with similar color and send
 it back to JOSM
 """
 
